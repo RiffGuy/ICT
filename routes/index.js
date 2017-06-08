7b9e24a0ff5094b4746cd1c1318da303ad36157d
RiffGuy <jins3569@gmail.com>
2017-06-08 19:35:44 +0900

    그래프 코드 분할

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,23 @@
 module.exports = function(app, Data){
 
   app.get('/', function(req,res){
-      res.render('index', {title:"aa", data: null});
+    Data.find().distinct('iotID').exec(function(err, data){
+        if(err) return res.status(500).json({error: err});
+        if(!data) return res.status(404).json({error: 'book not found'});
+        //res.json(data);
+        res.render('index', {title: "main", data: null, id_list : data });
+    });
   });
 
 //최근 데이터 128개 불러오기
   app.get('/api/data/:iot_id', function(req,res){
-    Data.find({iotID: req.params.iot_id}).sort({iot_date:-1}).limit(128).exec( function(err, data){
-      if(err) return res.status(500).json({error: err});
-      if(!data) return res.status(404).json({error: 'book not found'});
+      Data.find({iotID: req.params.iot_id}).sort({iot_date:-1}).limit(128).exec( function(err, data){
+          if(err) return res.status(500).json({error: err});
+          if(!data) return res.status(404).json({error: 'book not found'});
 //      res.json(data);
 
-      res.render('index', {title: req.params.iot_id, data: data });
-    });
+          res.render('index', {title: req.params.iot_id, data: data, id_list : null });
+      });
   });
 
 // 데이터값 추가
